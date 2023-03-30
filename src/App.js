74e41e97035c5a247930ee8f6e276ade20c9ec46
Yanirw <117165801+Yanirw@users.noreply.github.com>
2023-03-30 12:24:06 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p className='App-intro'>
           Deployed on AWS!
+            test change
         </p>
         <a
           className="App-link"
